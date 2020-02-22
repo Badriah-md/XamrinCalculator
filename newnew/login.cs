d91e57e8e2a86e5add8f74065d7f3abd2d385f4b
bdoor <bdoor92@gmail.com>
2020-02-22 18:48:54 +0300

    first

---
+++ newnew/login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace newnew
+{
+    public class login : ContentPage
+    {
+        public login()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ newnew/login.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace newnew
+{
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+    }
+}
